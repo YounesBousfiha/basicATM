7d50db11b7a383d1ecdf8fd5b2c540eddede28d5
Coderyounes <younesanas2021@gmail.com>
2025-09-03 21:18:18 +0100

    Add custom exception classes for insufficient balance and user not found scenarios

---
+++ src/main/java/org/example/domain/exceptions/InsufficientBalanceException.java
@@ -0,0 +1,8 @@
+package org.example.domain.exceptions;
+
+public class InsufficientBalanceException extends RuntimeException {
+
+    public InsufficientBalanceException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/org/example/domain/exceptions/UserNotFoundException.java
@@ -0,0 +1,8 @@
+package org.example.domain.exceptions;
+
+public class UserNotFoundException extends RuntimeException {
+
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
