b480194c5a0b75798a1f835ab2f6d4a7e1384785
Coderyounes <younesanas2021@gmail.com>
2025-09-03 19:51:30 +0100

    Add method to retrieve transactions in TransactionRepository

--- src/main/java/org/example/domain/IRepository/TransactionRepository.java
+++ src/main/java/org/example/domain/IRepository/TransactionRepository.java
@@ -1,6 +1,10 @@
 package org.example.domain.IRepository;
 
 import org.example.domain.models.Transaction;
+
+import java.util.concurrent.CopyOnWriteArrayList;
+
 public interface TransactionRepository {
     public Transaction save(Transaction transaction);
+    public CopyOnWriteArrayList<Transaction> getTransactions();
 }
