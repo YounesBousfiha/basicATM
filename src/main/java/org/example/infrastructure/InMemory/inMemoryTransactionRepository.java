94009c7b5afea0cc91c0b1ac8ed6d8dfe5f5f996
Coderyounes <younesanas2021@gmail.com>
2025-09-03 19:51:37 +0100

    Implement inMemoryTransactionRepository with save and getTransactions methods

--- src/main/java/org/example/infrastructure/InMemory/inMemoryTransactionRepository.java
+++ src/main/java/org/example/infrastructure/InMemory/inMemoryTransactionRepository.java
@@ -1,4 +1,22 @@
 package org.example.infrastructure.InMemory;
 
-public class inMemoryTransactionRepository {
+import org.example.domain.IRepository.TransactionRepository;
+import org.example.domain.models.Transaction;
+
+import java.util.concurrent.CopyOnWriteArrayList;
+
+public class inMemoryTransactionRepository implements TransactionRepository {
+
+    private final CopyOnWriteArrayList<Transaction> transactions = new CopyOnWriteArrayList<>();
+
+    @Override
+    public Transaction save(Transaction transaction) {
+        transactions.add(transaction);
+        return transaction;
+    }
+
+    @Override
+    public CopyOnWriteArrayList<Transaction> getTransactions() {
+        return transactions;
+    }
 }
