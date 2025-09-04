8f1246e13e9475d1e9f54ef60abed708ee703590
Coderyounes <younesanas2021@gmail.com>
2025-09-04 14:35:11 +0100

    Implement locking mechanism in withdraw and deposit methods; add negative amount check

--- src/main/java/org/example/infrastructure/Service/BankServiceImpl.java
+++ src/main/java/org/example/infrastructure/Service/BankServiceImpl.java
@@ -3,52 +3,73 @@ package org.example.infrastructure.Service;
 import org.example.domain.IRepository.AccountRepository;
 import org.example.domain.IRepository.TransactionRepository;
 import org.example.domain.exceptions.InsufficientBalanceException;
+import org.example.domain.exceptions.NegativeAmountException;
 import org.example.domain.exceptions.UserNotFoundException;
 import org.example.domain.models.Account;
 import org.example.domain.service.IBankService;
 
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class BankServiceImpl implements IBankService {
 
-    private final ReentrantLock lock = new ReentrantLock();
     private final AccountRepository accountRepo;
     private final TransactionRepository transactionRepo;
-
+    private final ConcurrentHashMap<Integer, ReentrantLock> accountLocks = new ConcurrentHashMap<>();
 
     public BankServiceImpl (AccountRepository accountRepo, TransactionRepository transactionRepo) {
         this.accountRepo = accountRepo;
         this.transactionRepo = transactionRepo;
     }
 
+
+    private ReentrantLock getLockAccount(int accountNumber) {
+        return accountLocks.computeIfAbsent(accountNumber, k -> new ReentrantLock());
+    }
+
     @Override
     public  void  withdraw(int accountNumber, double amount) {
-        if(lock.tryLock()) {
-            try {
-                Account account = accountRepo.findByAccountNumber(accountNumber);
-                if(account.equals(null)) {
-                    throw new UserNotFoundException("User not found");
-                }
-
-                if(account.getBalance() < amount) {
-                    throw new InsufficientBalanceException("No enough money in your account");
-                } else {
-                    account.setBalance(account.getBalance() - amount);
-                    accountRepo.update(account);
-                }
-            } catch (UserNotFoundException | InsufficientBalanceException e) {
-                System.out.println(e.getMessage());
-            } finally {
-                lock.unlock();
+        ReentrantLock lock = getLockAccount(accountNumber);
+        lock.lock();
+        try {
+            Account account = accountRepo.findByAccountNumber(accountNumber);
+            if(java.util.Objects.isNull(account)) {
+                throw new UserNotFoundException("user not Found");
             }
-        } else {
-            System.out.println("Couldn't Get the Lock");
+            double balance = account.getBalance();
+            if(balance < amount) {
+                throw new InsufficientBalanceException("Insufficient Balance");
+            }
+
+            account.setBalance(balance - amount);
+            accountRepo.update(account);
+        } catch (UserNotFoundException | InsufficientBalanceException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            lock.unlock();
         }
     }
 
     @Override
     public  void deposit(int accountNumber, double amount) {
+        ReentrantLock lock = getLockAccount(accountNumber);
+        lock.lock();
 
+        try {
+            Account account = accountRepo.findByAccountNumber(accountNumber);
+            if(java.util.Objects.isNull(account)) {
+                throw new UserNotFoundException("User not Found!");
+            }
+            if(amount < 0) {
+                throw new NegativeAmountException("Amount is negative");
+            }
+            account.setBalance(account.getBalance() + amount);
+            accountRepo.update(account);
+        } catch (UserNotFoundException | NegativeAmountException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            lock.unlock();
+        }
     }
 
     @Override
