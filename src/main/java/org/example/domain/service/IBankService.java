68d85e1cf3412e1d408a74c74af4d646cb9f1a22
Coderyounes <younesanas2021@gmail.com>
2025-09-04 17:12:20 +0100

    Add authentication method and update withdraw/deposit signatures in IBankService

--- src/main/java/org/example/domain/service/IBankService.java
+++ src/main/java/org/example/domain/service/IBankService.java
@@ -1,8 +1,9 @@
 package org.example.domain.service;
 
 public interface IBankService {
-    public void withdraw(int accountNumber, double amount);
-    public void deposit(int accountNumber, double amount);
+    public boolean authenticate(int accountNumber, int pin);
+    public boolean withdraw(int accountNumber, double amount);
+    public boolean deposit(int accountNumber, double amount);
     public void transfer(int fromAccountNumber, int toAccountNumber, double amount);
     public double getBalance(int accountNumber);
 }
