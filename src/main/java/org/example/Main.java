905428a7f1a3528a0352d6b3ccdaa4757855c433
Coderyounes <younesanas2021@gmail.com>
2025-09-04 19:40:50 +0100

    Implement main application logic to initialize repositories, start concurrent sessions, and manage user interactions

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,31 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import org.example.application.controller.ConcurrentSessionManager;
+import org.example.application.ui.ConsoleUI;
+import org.example.domain.IRepository.AccountRepository;
+import org.example.domain.IRepository.TransactionRepository;
+import org.example.domain.models.Account;
+import org.example.domain.service.IBankService;
+import org.example.infrastructure.InMemory.InMemoryAccountRepository;
+import org.example.infrastructure.InMemory.inMemoryTransactionRepository;
+import org.example.infrastructure.Service.BankServiceImpl;
+
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        AccountRepository accountRepo = new InMemoryAccountRepository();
+        TransactionRepository transactionRepo = new inMemoryTransactionRepository();
+        IBankService bankService = new BankServiceImpl(accountRepo, transactionRepo);
+        ConsoleUI consoleUI = new ConsoleUI();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+
+        accountRepo.save(new Account(123, 1111, 1000.0));
+
+        ConcurrentSessionManager manager = new ConcurrentSessionManager(3, bankService, consoleUI);
+        manager.startSessions(3);
+
+
+
+        manager.shutdown();
     }
 }
