affc0f912560ce4f62249f6a20e0b2f03567ed10
Coderyounes <younesanas2021@gmail.com>
2025-09-04 16:29:09 +0100

    Add ConsoleUI class for user interaction; implement methods for reading account details, showing balance, and displaying menu options

--- src/main/java/org/example/application/ui/ConsoleUI.java
+++ src/main/java/org/example/application/ui/ConsoleUI.java
@@ -1,4 +1,57 @@
 package org.example.application.ui;
 
+import java.util.Scanner;
+
 public class ConsoleUI {
+
+    private Scanner scanner = new Scanner(System.in);
+
+    public String readAccountNumber() {
+        System.out.print("Enter Account Number");
+        return scanner.nextLine();
+    }
+
+    public String readPin() {
+        System.out.print("Enter PIN: ");
+        return scanner.nextLine();
+    }
+
+    public double readAmount() {
+        System.out.println("Enter Amount: ");
+        while(!scanner.hasNextDouble()) {
+            System.out.print("Invalid. Enter numeric amount: ");
+            scanner.next();
+        }
+        return scanner.nextDouble();
+    }
+
+    public void showBalance(double balance) {
+        System.out.println("Current balance: " + balance);
+    }
+
+    public void showMessage(String message) {
+        System.out.println(message);
+    }
+
+    public int showMenu() {
+        System.out.println("\n1. Check Balance");
+        System.out.println("2. Withdraw");
+        System.out.println("3. Deposit");
+        System.out.println("4. Transfer");
+        System.out.println("0. Exit");
+
+        System.out.print("Choose option: ");
+        return scanner.nextInt();
+    }
+
+    public void showError(String error) {
+        System.err.println("Error: " + error);
+    }
+
+    public String readTargetAccount() {
+        scanner.nextLine();
+        System.out.print("Enter Target account number: ");
+        return scanner.nextLine();
+    }
+
 }
